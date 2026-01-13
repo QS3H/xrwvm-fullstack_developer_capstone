95870bd96002b7f68f05629166a9ec74ca2942ba
shawkyahmad <shawkyahmad703@gmail.com>
2026-01-13 06:45:03 -0500

    add register route path

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,16 @@
-import LoginPanel from "./components/Login/Login"
+import React from "react";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
+      {/* Add other routes here as needed */}
     </Routes>
   );
 }
+
 export default App;
